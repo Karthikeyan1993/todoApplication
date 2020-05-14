bf9e11a4b67cc39f656d80d37f1f38cbb0f089b5
Karthikeyan1993 <karthik.infotec99@gmail.com>
2020-05-15 03:36:26 +0530

    data grid UI Changes

--- src/app/data-grid/data-grid.component.ts
+++ src/app/data-grid/data-grid.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {Todo, ColumnDef} from '../shared/model';
 
 @Component({
   selector: 'app-data-grid',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-grid.component.css']
 })
 export class DataGridComponent implements OnInit {
+  @Input() todoList: Todo[];
+  @Input() columnDefs: ColumnDef[];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  getPriorityClass = val => {
+    return {
+      'flag-high': val === 'High',
+      'flag-medium': val === 'Medium',
+      'flag-low': val === 'Low'
+    }
+  }
+
+  getStatusClass = val => {
+    return {
+      'badge-danger': val === 'Overdue',
+      'badge-success': val === 'Completed',
+      'badge-primary': val === 'In Progress'
+    }
   }
 
 }
