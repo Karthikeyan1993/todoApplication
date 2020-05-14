902383314970a5fbc58430bf5b5e7d754da64c58
Karthikeyan1993 <karthik.infotec99@gmail.com>
2020-05-14 19:32:59 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
